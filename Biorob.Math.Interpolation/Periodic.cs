354fea9cd0a0795d7e7d94709f5fe658cb09a76e
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2011-06-16 15:16:23 +0200

    Added pchip interpolation

---
+++ Biorob.Math.Interpolation/Periodic.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+namespace Biorob.Math.Interpolation
+{
+	public class Periodic
+	{
+		public static void Extend(List<Point> points, double min, double max)
+		{
+			if (points.Count < 2)
+			{
+				return;
+			}
+
+			double period = max - min;
+			int size = points.Count;
+			
+			Point first = points[0];
+			Point second = points[1];
+			
+			Point last = points[size - 1];
+			Point prelast = points[size - 2];
+			
+			points.Insert(0, new Point(last.X - period, last.Y));
+			points.Insert(0, new Point(prelast.X - period, last.Y));
+			
+			points.Add(new Point(first.X + period, first.Y));
+			points.Add(new Point(second.X + period, second.Y));
+		}
+	}
+}
+
