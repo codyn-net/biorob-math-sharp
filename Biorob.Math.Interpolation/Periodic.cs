9a6995ab4948bf8ea2483c822b011b867dff8e38
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2011-09-07 19:06:45 +0200

    Fixed periodic extension

--- Biorob.Math.Interpolation/Periodic.cs
+++ Biorob.Math.Interpolation/Periodic.cs
@@ -5,6 +5,14 @@ namespace Biorob.Math.Interpolation
 {
 	public class Periodic
 	{
+		public static List<Point> Extend(Point[] points, double min, double max)
+		{
+			List<Point> ret = new List<Point>(points);
+			Extend(ret, min, max);
+			
+			return ret;
+		}
+
 		public static void Extend(List<Point> points, double min, double max)
 		{
 			if (points.Count < 2)
@@ -22,7 +30,7 @@ namespace Biorob.Math.Interpolation
 			Point prelast = points[size - 2];
 			
 			points.Insert(0, new Point(last.X - period, last.Y));
-			points.Insert(0, new Point(prelast.X - period, last.Y));
+			points.Insert(0, new Point(prelast.X - period, prelast.Y));
 			
 			points.Add(new Point(first.X + period, first.Y));
 			points.Add(new Point(second.X + period, second.Y));
